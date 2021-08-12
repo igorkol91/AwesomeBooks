0e9f83a6e202a75195fb4fc403d180f41efc9eaa
Igor.Koloski <koloski_igor@hotmail.com>
2021-08-12 11:42:54 +0200

    Add logic for main and create new page dom manipulation

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import Add from './module/add.js';
 
 const add = new Add();
 const remove = new Remove();
-const createNew = document.querySelector('#createNewButton');
-const bookContainer = document.querySelector('#book-container');
+const bookContainer = document.createElement('ul');
+const mainContainer = document.querySelector('main');
+const mainPage = document.querySelector('#main-page');
+const createNewPage = document.querySelector('#create-new-page');
 let allBooks = [];
 if (localStorage.getItem('bookList') === null) {
   localStorage.setItem('bookList', []);
@@ -13,6 +15,7 @@ if (localStorage.getItem('id') === null) {
   localStorage.setItem('id', JSON.stringify(0));
 }
 const refreshDOM = () => {
+  mainContainer.innerHTML = '';
   allBooks = JSON.parse(localStorage.getItem('bookList'));
   allBooks.forEach((book) => {
     const bookTitle = book.name;
@@ -26,18 +29,56 @@ const refreshDOM = () => {
     });
     const newBook = document.createElement('li');
     const newTitle = document.createElement('p');
-    newBook.classList += 'd-flex w-50 justify-content-between';
+    newBook.classList += 'd-flex w-50 justify-content-between pb-4';
     newTitle.innerHTML = `${bookTitle} by ${bookAuthor}`;
     newBook.id = bookId;
     newBook.appendChild(newTitle);
     newBook.appendChild(removeBtn);
     bookContainer.appendChild(newBook);
+    bookContainer.id = 'book-container';
+    bookContainer.classList += 'w-50 d-flex flex-column align-items-center';
+    mainContainer.appendChild(bookContainer);
   });
 };
 window.onload = refreshDOM;
-createNew.addEventListener('click', (e) => {
-  e.preventDefault();
-  add.add();
-  bookContainer.innerHTML = '';
+
+// Create main page with books list
+
+mainPage.addEventListener('click', () => {
   refreshDOM();
+});
+
+// Create the form and adding books logic on the Add page
+
+createNewPage.addEventListener('click', (e) => {
+  e.preventDefault();
+  mainContainer.innerHTML = '';
+  const form = document.createElement('form');
+  const titleInput = document.createElement('input');
+  titleInput.type = 'text';
+  titleInput.name = 'bookname';
+  titleInput.id = 'bookname';
+  titleInput.placeholder = 'Title';
+  form.appendChild(titleInput);
+
+  const authorInput = document.createElement('input');
+  authorInput.type = 'text';
+  authorInput.name = 'authorname';
+  authorInput.id = 'authorname';
+  authorInput.placeholder = 'Author';
+  form.appendChild(authorInput);
+
+  const addBTN = document.createElement('button');
+  addBTN.id = 'createNewButton';
+  addBTN.type = 'button';
+  addBTN.innerText = 'Submit';
+  form.appendChild(addBTN);
+
+  mainContainer.appendChild(form);
+
+  addBTN.addEventListener('click', () => {
+    add.add(titleInput.value, authorInput.value);
+    titleInput.value = '';
+    authorInput.value = '';
+  });
 });
